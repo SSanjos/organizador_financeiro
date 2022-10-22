19108be4f72df14d3c968b1fc12b646cb8dba9f6
SSanjos <sandro.sennaanjos@gmail.com>
2022-10-22 00:15:22 -0300

    commit inicial

---
+++ src/main/java/br/com/g6/organizadorfinanceiro/OrganizadorfinanceiroApplication.java
@@ -0,0 +1,13 @@
+package br.com.g6.organizadorfinanceiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizadorfinanceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganizadorfinanceiroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/g6/organizadorfinanceiro/OrganizadorfinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.g6.organizadorfinanceiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizadorfinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
