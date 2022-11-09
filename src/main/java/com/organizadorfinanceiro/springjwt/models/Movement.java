6b405271c939b463e9192e47a0c29a9860d96426
Letícia Costa <leticiaccmarques@hotmail.com>
2022-11-09 07:59:47 -0300

    Add jwt authentication

---
+++ src/main/java/com/organizadorfinanceiro/springjwt/models/Movement.java
@@ -0,0 +1,69 @@
+package com.organizadorfinanceiro.springjwt.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.organizadorfinanceiro.springjwt.enumeration.TypeMovement;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+
+@Entity
+@Table(name = "Movement")
+public class Movement {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Getter
+	@Setter //??
+//	@Column(name = "idMovement", nullable = false)//permite alterar nome da coluna
+	private Long idMovement;
+
+//	@NotNull
+//	@Getter
+//	@Setter
+//	@Enumerated(EnumType.STRING)
+//	private TypeMovement typeMovement;
+
+	
+	@NotNull
+	@Getter
+	@Setter
+	private double valueMovement;
+	
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	@Getter
+	@Setter
+	private Date dateMovement;
+	
+	@NotNull
+	@Getter
+	@Setter
+	@Size(min = 1, max = 500)
+	private String descriptionMovement;
+
+
+	@Getter
+	@Setter
+	private int seqParcel;
+	//retirar??
+
+	@ManyToOne
+	@JsonIgnoreProperties ("movementList")
+	@JoinColumn ( name = "id")
+	@Getter
+	@Setter
+	private User user;
+
+
+	@NotNull
+	@Getter
+	@Setter
+	@Enumerated(EnumType.STRING)
+	private TypeMovement typeMovement;
+	}
+
